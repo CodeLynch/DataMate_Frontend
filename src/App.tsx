d7070012ec6e923662aa86b16ba36803a6828740
citmarielgenodiala <genodialamariel1701@gmail.com>
2023-05-19 20:05:43 +0800

    update app.tsx
    just to test the snackbar-navbar-topbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,68 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Routes } from 'react-router-dom';
+import SnackbarContextProvider from './helpers/SnackbarContext';
+import Topbar from './components/Topbar';
+import Navbar from './components/Navbar';
+import { ThemeProvider } from '@mui/material/styles';
+import { PaletteColorOptions, createTheme } from '@mui/material/styles';
+import { Modal } from '@mui/material';
+
+
+/* Customize default MUI theme */
+declare module '@mui/material/styles' {
+  interface PaletteOptions {
+    tertiary?: PaletteColorOptions;
+  }
+}
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#0E9EA7',
+      contrastText: '#fff',
+    },
+    secondary: {
+      main: "#71C887",
+      contrastText: '#fff',
+    },
+    tertiary: {
+      main: '#DCF1EC',
+      contrastText: '#374248',
+    },
+  },
+  
+  typography: {
+    fontFamily: [
+      'inter',
+      'sans-serif',
+    ].join(','),
+  },
+});
 
 function App() {
+  const [open, setOpen] = useState(false);
+
+  const handleDrawerOpen = () => {
+    setOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+    setOpen(false);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <SnackbarContextProvider>
+        <Router>
+          <Modal open={open} onClose={handleDrawerClose}>
+            <Navbar open={open} handleDrawerClose={handleDrawerClose} />
+          </Modal>
+          <Topbar open={open} handleDrawerOpen={handleDrawerOpen} />
+          <Routes>
+            {/* Add your routes here */}
+          </Routes>
+        </Router>
+      </SnackbarContextProvider>
+    </ThemeProvider>
   );
 }
 
