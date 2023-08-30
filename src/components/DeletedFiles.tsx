94035919620c20e0ae7f098eb270d700f6b03fd7
Cara Carmel Encabo <caracarmel.encabo@cit.edu>
2023-08-30 09:34:43 +0800

    Update deleted file page

--- src/components/DeletedFiles.tsx
+++ src/components/DeletedFiles.tsx
@@ -1,12 +1,17 @@
 import React, { useState } from 'react';
-import { Box, Button, Checkbox, FormControl, FormControlLabel, FormGroup, Grid, InputLabel, MenuItem, Select, SelectChangeEvent, TextField, Typography } from '@mui/material';
+import { Box, Button, Checkbox, FormControl, FormControlLabel, FormGroup, Grid, IconButton, InputLabel, Menu, MenuItem, Select, SelectChangeEvent, Stack, TextField, Typography, useMediaQuery, useTheme } from '@mui/material';
 import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
 import InputAdornment from '@mui/material/InputAdornment';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
 
 export default function DeletedFiles() {
     const [isLabelShrunk, setIsLabelShrunk] = useState(false);
     const [selectedOption, setSelectedOption] = useState('');
     const [sort, setSort] = React.useState('');
+    const [menuAnchor, setMenuAnchor] = React.useState<null | HTMLElement>(null);
+
+    const theme = useTheme();
+    const isNotXsScreen = useMediaQuery(theme.breakpoints.up('sm'));
 
     const handleChange = (event: SelectChangeEvent) => {
         setSort(event.target.value as string);
@@ -32,17 +37,23 @@ export default function DeletedFiles() {
         }
     };
 
+    const handleMenuClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+        setMenuAnchor(event.currentTarget);
+    };
+
+    const handleMenuClose = () => {
+        setMenuAnchor(null);
+    };
+
     return (
         <div>
             <Box m={4}>
                 <Typography variant="h4" fontWeight="bold" color="#374248"> Deleted Files </Typography>
             </Box>
-
-            <Box mx={4}>
-                <Grid container justifyContent="space-between" alignItems="center">
-                    <Grid container item xs={12} sm={6} md={7} spacing={3} alignItems="center">
-                        <Grid item xs={12} sm={6} md={6}>
-                            <TextField 
+            <Stack direction="row" mx={4}>
+                <Grid container>
+                    <Grid item xs={15} sm={11} md={11}>
+                        <TextField 
                                 id="outlined-search" 
                                 label="Search" 
                                 type="search"
@@ -68,72 +79,151 @@ export default function DeletedFiles() {
                                     },
                                     width: '100%'
                                 }}
-                            />
-                        </Grid>
-                        <Grid item xs={12} sm={6} md={5} mt={1} ml={2}>
-                            <Grid container direction="row" alignItems="center" spacing={1}>
-                                <FormControlLabel
-                                    value="delete"
-                                    control={<Checkbox />}
-                                    label="Delete Forever"
-                                    labelPlacement="end"
-                                    checked={selectedOption === 'delete'}
-                                    onChange={handleCheckboxChange}
-                                />
-                                <FormControlLabel
-                                    value="restore"
-                                    control={<Checkbox />}
-                                    label="Restore Files"
-                                    labelPlacement="end"
-                                    checked={selectedOption === 'restore'}
-                                    onChange={handleCheckboxChange}
-                                />
-                            </Grid>
-                        </Grid>
-                    </Grid>
-                    <Grid container item xs={12} sm={6} md={3.5} spacing={2} justifyContent="flex-end">
-                        <Grid item xs={12} sm={6} md={4}>
-                            <FormControl fullWidth>
-                                <InputLabel id="demo-simple-select-label">Sort by</InputLabel>
-                                <Select
-                                    labelId="demo-simple-select-label"
-                                    id="demo-simple-select"
-                                    value={sort}
-                                    label="Sort by"
-                                    onChange={handleChange}
-                                    sx={{
-                                        backgroundColor: '#A5E6AC',
-                                        '& .MuiOutlinedInput-notchedOutline': {
-                                            border: 'none',
-                                        },
-                                        '& .MuiSelect-root': {
-                                            zIndex: 1,
-                                        },
-                                    }}
-                                >
-                                    <MenuItem value='all'>All</MenuItem>  {/* what are the menu items? */}
-                                    <MenuItem value='name'>Name</MenuItem>
-                                    <MenuItem value='date'>Date</MenuItem>
-                                </Select>
-                            </FormControl>
-                        </Grid>
-                        <Grid item xs={12} sm={6} md={4}>
-                        <Button
-                            sx={{
-                                fontSize: '13px',
-                                color: 'red',
-                                mt: { xs: -1, sm: 1, md: 1 },
-                                textTransform: 'none',
-                            }}
-                            variant="text"
-                        >
-                            Clear Filters
-                        </Button>
-
-                        </Grid>
+                        />
                     </Grid>
                 </Grid>
-            </Box>
+                {isNotXsScreen ? (
+                    <>
+                        <Grid container mr={1}>
+                            <Grid item md={12}>
+                                <Stack direction="row" spacing={2}>
+                                    <FormControlLabel
+                                        value="delete"
+                                        control={<Checkbox />}
+                                        label="Delete Forever"
+                                        labelPlacement="end"
+                                        checked={selectedOption === 'delete'}
+                                        onChange={handleCheckboxChange}
+                                    />
+                                    <FormControlLabel
+                                        value="restore"
+                                        control={<Checkbox />}
+                                        label="Restore Files"
+                                        labelPlacement="end"
+                                        checked={selectedOption === 'restore'}
+                                        onChange={handleCheckboxChange}
+                                    />
+                                </Stack>
+                            </Grid>
+                        </Grid>
+
+                        <Grid container direction="row" justifyContent="flex-end" alignItems="flex-end">
+                            <Grid item sm={10} md={7}>
+                                <Stack direction="row" spacing={2}>
+                                    <FormControl fullWidth>
+                                        <InputLabel id="demo-simple-select-label">Sort by</InputLabel>
+                                        <Select
+                                            labelId="demo-simple-select-label"
+                                            id="demo-simple-select"
+                                            value={sort}
+                                            label="Sort by"
+                                            onChange={handleChange}
+                                            sx={{
+                                                backgroundColor: '#A5E6AC',
+                                                '& .MuiOutlinedInput-notchedOutline': {
+                                                    border: 'none',
+                                                },
+                                                '& .MuiSelect-root': {
+                                                    zIndex: 1,
+                                                },
+                                            }}
+                                        >
+                                            <MenuItem value='all'>All</MenuItem>
+                                            <MenuItem value='name'>Name</MenuItem>
+                                            <MenuItem value='date'>Date</MenuItem>
+                                        </Select>
+                                    </FormControl>
+                                    <Grid item sm={15} md={10}>
+                                        <Button
+                                            sx={{
+                                                fontSize: '13px',
+                                                color: 'red',
+                                                textTransform: 'none',
+                                                mt: 1
+                                            }}
+                                            variant="text"
+                                        >
+                                            Clear Filters
+                                        </Button>
+                                    </Grid>
+                                </Stack>
+                            </Grid>
+                        </Grid>
+                    </>
+                ) : (
+                    <Box mt={1}> {/* with menu icon for xs screens */}
+                        <Grid container> 
+                            <IconButton onClick={handleMenuClick}>
+                                <MoreVertIcon />
+                            </IconButton>
+                            <Menu
+                                anchorEl={menuAnchor}
+                                open={Boolean(menuAnchor)}
+                                onClose={handleMenuClose}
+                            >
+                                <MenuItem>
+                                    <FormControlLabel
+                                        value="delete"
+                                        control={<Checkbox />}
+                                        label="Delete Forever"
+                                        labelPlacement="end"
+                                        checked={selectedOption === 'delete'}
+                                        onChange={handleCheckboxChange}
+                                    />
+                                </MenuItem>
+                                <MenuItem>
+                                    <FormControlLabel
+                                        value="restore"
+                                        control={<Checkbox />}
+                                        label="Restore Files"
+                                        labelPlacement="end"
+                                        checked={selectedOption === 'restore'}
+                                        onChange={handleCheckboxChange}
+                                    />
+                                </MenuItem>
+                                <MenuItem>
+                                    <FormControl fullWidth>
+                                        <InputLabel id="demo-simple-select-label">Sort by</InputLabel>
+                                        <Select
+                                            labelId="demo-simple-select-label"
+                                            id="demo-simple-select"
+                                            value={sort}
+                                            label="Sort by"
+                                            onChange={handleChange}
+                                            sx={{
+                                                backgroundColor: '#A5E6AC',
+                                                '& .MuiOutlinedInput-notchedOutline': {
+                                                    border: 'none',
+                                                },
+                                                '& .MuiSelect-root': {
+                                                    zIndex: 1,
+                                                },
+                                            }}
+                                        >
+                                            <MenuItem value='all'>All</MenuItem>
+                                            <MenuItem value='name'>Name</MenuItem>
+                                            <MenuItem value='date'>Date</MenuItem>
+                                        </Select>
+                                    </FormControl>
+                                </MenuItem>
+                                <MenuItem>
+                                    <Button
+                                        fullWidth
+                                        sx={{
+                                            fontSize: '13px',
+                                            color: 'red',
+                                            textTransform: 'none',
+                                        }}
+                                        variant="text"
+                                    >
+                                        Clear Filters
+                                    </Button>
+                                </MenuItem>
+                            </Menu>
+                        </Grid>
+                    </Box>
+                    )}
+            </Stack>
         </div>
     );
 }
