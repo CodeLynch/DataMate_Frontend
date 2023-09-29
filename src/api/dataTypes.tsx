8c49a453f6f9c7cfacdd57ae7d976a8ae8c81b72
Cara Carmel Encabo <caracarmel.encabo@cit.edu>
2023-09-29 17:00:24 +0800

    Add user data type in dataTypes.tsx

--- src/api/dataTypes.tsx
+++ src/api/dataTypes.tsx
@@ -1,5 +1,17 @@
 /*add your dataTypes here*/
 
+export type User = {
+  userId: number,
+  firstName: string,
+  lastName: string,
+  email: string,
+  address: string,
+  username: string,
+  password: string,
+  businessName: string,
+  businessType: string,
+}
+
 export interface FileEntity {
   fileId: number;
   fileName: string;
