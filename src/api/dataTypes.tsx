96fa6e8d141a07c99afb8cf7be5c2a82241f0748
Cara Carmel Encabo <caracarmel.encabo@cit.edu>
2023-10-11 17:41:36 +0800

    Update dataTypes

--- src/api/dataTypes.tsx
+++ src/api/dataTypes.tsx
@@ -1,16 +1,16 @@
 /*add your dataTypes here*/
 
-// export type User = {
-//   userId: number,
-//   firstName: string,
-//   lastName: string,
-//   email: string,
-//   address: string,
-//   username: string,
-//   password: string,
-//   businessName: string,
-//   businessType: string,
-// }
+export type User = {
+  userId: number,
+  firstName: string,
+  lastName: string,
+  email: string,
+  address: string,
+  username: string,
+  password: string,
+  businessName: string,
+  businessType: string,
+}
 
 export interface FileEntity {
   fileId: number;
