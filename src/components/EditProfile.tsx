253b26ba5a02221312a95a34bcf22967ca29b3ac
Cara Carmel Encabo <caracarmel.encabo@cit.edu>
2023-11-17 10:07:42 +0800

    Update decryption for EditProfile

--- src/components/EditProfile.tsx
+++ src/components/EditProfile.tsx
@@ -100,11 +100,14 @@ export default function EditProfile() {
     const putUser = async (e: { preventDefault: () => void; }) => {
         e.preventDefault()
 
+        const ENCRYPTION_KEY = process.env.REACT_APP_ENCRYPTION_KEY || 'DefaultKey';
+        const decryptedUserId = CryptoJS.AES.decrypt(userId, ENCRYPTION_KEY).toString(CryptoJS.enc.Utf8);
+
         if (passwordLengthError) {
             return;
         }
 
-        await axios.put(`https://datamate-api.onrender.com/user/putUser?userId=${userId}`,{
+        await axios.put(`https://datamate-api.onrender.com/user/putUser?userId=${decryptedUserId}`,{
             firstName: data.firstName,
             lastName: data.lastName,
             email: data.email,
